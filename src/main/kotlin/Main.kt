a667e0f2ca89c808c7ddb08d90f32706e5f22b2b
44x1carbon <b4079@oic.jp>
2018-03-26 10:09:14 +0900

    kotlinx.htmlを設定

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,26 @@
 import io.ktor.application.*
-import io.ktor.http.*
+import io.ktor.html.respondHtml
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import kotlinx.html.*
 
 fun main(args: Array<String>) {
     val server = embeddedServer(Netty, port = 8080) {
         routing {
             get("/") {
-                call.respondText("Hello World!", ContentType.Text.Plain)
+                call.respondHtml {
+                    head {
+                        title { +"Async World" }
+                    }
+                    body {
+                        h1 {
+                            id = "title"
+                            +"Title"
+                        }
+                    }
+                }
             }
             get("/demo") {
                 call.respondText("HELLO WORLD!")
